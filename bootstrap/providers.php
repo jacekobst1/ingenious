925f2facf95d948aa6e872bb975316946558018f
Jacek Obst <jacekobst1@gmail.com>
2025-10-30 19:04:10 +0100

    chore: install and configure laravel/telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    \Modules\Notifications\Infrastructure\Providers\NotificationServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
+    Modules\Notifications\Infrastructure\Providers\NotificationServiceProvider::class,
 ];
